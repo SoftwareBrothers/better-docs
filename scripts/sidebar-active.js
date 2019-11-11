c34824a5c993eac00ef8a5ce981a72da7e48db8e
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-11-11 22:20:35 +0100

    improve styling

---
+++ scripts/sidebar-active.js
@@ -0,0 +1,8 @@
+$().ready(() => {
+  $('#sidebarNav a').each((index, el) => {
+    const href = $(el).attr('href');
+    if (window.location.pathname.match(href)) {
+      $(el).addClass('active')
+    }
+  })
+})
