a14195adc716a3952814f5810e9c46d39a339dba
Jonas <login@jonas-brueggen.de>
2020-09-11 14:27:40 +0200

    Update component.js
    added reactDocs resolver "findAllComponentDefinitions", to fix problem with multiple exported components in single file

--- component.js
+++ component.js
@@ -55,7 +55,7 @@ var parseReact = function (filePath, doclet) {
   var src = fs.readFileSync(filePath, 'UTF-8')
   var docGen
   try {
-    docGen = reactDocs.parse(src)
+    docGen = reactDocs.parse(src, reactDocs.resolver.findAllComponentDefinitions)
   } catch (error) {
     if (error.message === 'No suitable component definition found.') {
       return {
