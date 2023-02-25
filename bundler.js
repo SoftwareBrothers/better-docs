dd8bdb42b3a274bf85a71bcc3c9cd84a45c4a8a6
njetsy <njetsy@gmail.com>
2023-02-25 14:12:57 -0500

    Fixes for parcel v2 bundling

--- bundler.js
+++ bundler.js
@@ -78,7 +78,9 @@ module.exports = function bundle (Components, out, config) {
 
   console.log('Generating entry file for "components" plugin')
   fs.writeFileSync(entry, entryFile)
-  const cmd = `${process.platform === 'win32' ? 'SET ' : ''}NODE_ENV=development parcel build ${entry} --dist-dir ${out}`
+  console.log('Bundling components...')
+  const outDist = path.join(out, 'build')
+  const cmd = `parcel build ${entry} --dist-dir ${outDist} --no-optimize`
   console.log(`running: ${cmd}`)
   try {
     execSync(cmd)
